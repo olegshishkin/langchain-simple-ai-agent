4f17869f5b8fd4b1349137929608ab2e5ef239c8
olegshishkin <olegshishkin@gmail.com>
2025-01-25 20:07:20 +0300

    commit

--- src/main/java/org/github/olegshishkin/ai/agent/MyController.java
+++ src/main/java/org/github/olegshishkin/ai/agent/MyController.java
@@ -1,6 +1,7 @@
 package org.github.olegshishkin.ai.agent;
 
 import lombok.RequiredArgsConstructor;
+import org.github.olegshishkin.ai.agent.MyAssistant.ElephantCountRs;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ public class MyController {
     private final MyAssistant assistant;
 
     @PostMapping("/assist")
-    public String assist(@RequestBody String prompt) {
+    public ElephantCountRs assist(@RequestBody String prompt) {
         return assistant.assist(prompt);
     }
 }
